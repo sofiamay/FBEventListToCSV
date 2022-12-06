8730ff94feaf2c9b75a7f0ee09e152cea3e731d7
Sofia Berlin <sofiamberlin@gmail.com>
2022-12-05 18:24:45 -0800

    create a script that exports fb guest list to csv file

---
+++ get-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+function getUsers() {
+	try {
+		let rawdata = fs.readFileSync('user-info.txt');
+		let userInfo = JSON.parse(rawdata);
+		let users = userInfo.event_connected_users_private.nodes;
+		if (users.length == 0) {
+			console.log("No users going to event");
+			return false;
+		}
+		_writeToFile(users);
+		return true;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+}
+
+function _writeToFile(users) {
+	const header = "User ID,name\n";
+	let file = fs.createWriteStream('guest-list.txt');
+	file.write(header);
+
+	users.forEach(function(user) {
+		let id = "";
+		let name = "";
+
+		if ('id' in user) {
+			id = user.id;
+		}
+		if ('name' in user) {
+			name = user.name;
+		}
+		//write to file:
+		file.write(`${id},${name}\n`);
+	});
+
+	file.end();
+}
+
+
+// RUN:
+getUsers();
