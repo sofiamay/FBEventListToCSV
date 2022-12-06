5b88911ffd9677ea9efa4fe09fc810f3a983fcdb
Sofia Berlin <sofiamberlin@gmail.com>
2022-12-05 19:35:42 -0800

    node script takes an html source file of a FB event and outputs attendees to a csv file

--- get-users.js
+++ get-users.js
@@ -2,28 +2,50 @@
 
 const fs = require('fs');
 
+const INPUT_PATH = process.env.INPUT_PATH || "source.html";
+const OUTPUT_PATH = process.env.OUTPUT_PATH || "guest-list.csv";
 
-// takes user info from user-info filt and exports to guest-list.csv file
-function getUsersFromJSONFile() {
+// Extracts users attending a facebook event and exports them to a CSV file. 
+// Save the facebook event page to INPUT_PATH or replace source.html with the fb event page
+// results are saved to OUTPUT_PATH or "guest-list.csv"
+// and are in the format "User Id, name"
+function getUsersFromHTML() {
 	try {
-		let rawdata = fs.readFileSync('user-info.txt');
-		let userInfo = JSON.parse(rawdata);
-		let users = userInfo.event_connected_users_private.nodes;
-		if (users.length == 0) {
-			console.log("No users going to event");
+		const html = fs.readFileSync(INPUT_PATH).toString();
+		const regex = /"event_connected_users_private.+]},"event_connected_emails_going"/;
+		const found = html.match(regex);
+		if (found.length != 1) {
+			console.log("Cannot find list of attendees within html file");
 			return false;
 		}
-		_writeToFile(users);
-		return true;
+		let match = found[0];
+		// remove next JSON opbject from match string
+		match = match.replace(`,"event_connected_emails_going"`, "");
+		// enclose match in {} to make valid JSON string
+		const data = "{" + match + "}";
+		return getUsers(data);
 	} catch (error) {
 		console.log(error);
 		return false;
 	}
 }
 
+// takes user info string and exports to csv file
+function getUsers(data) {
+	let userInfo = JSON.parse(data);
+	let users = userInfo.event_connected_users_private.nodes;
+	if (users.length == 0) {
+		console.log("No users going to event");
+		return false;
+	}
+	_writeToFile(users);
+	return true;
+}
+
+// takes a JSON list of users and outputs their info into OUTPUT_PATh csv file
 function _writeToFile(users) {
 	const header = "User ID,name\n";
-	let file = fs.createWriteStream('guest-list.csv');
+	let file = fs.createWriteStream(OUTPUT_PATH);
 	file.write(header);
 
 	users.forEach(function(user) {
@@ -45,4 +67,4 @@ function _writeToFile(users) {
 
 
 // RUN:
-getUsersFromJSONFile();
+getUsersFromHTML();
