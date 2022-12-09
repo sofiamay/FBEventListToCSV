cf04df07dda980d6e66bb47dabc73e5dc3965741
Sofia Berlin <sofiamberlin@gmail.com>
2022-12-09 14:08:57 -0800

    Add options to cli tool

--- guest-list-to-csv.js
+++ guest-list-to-csv.js
@@ -1,17 +1,15 @@
 'use strict';
-
 const fs = require('fs');
-
-const INPUT_PATH = process.env.INPUT_PATH || "source.html";
-const OUTPUT_PATH = process.env.OUTPUT_PATH || "guest-list.csv";
 
 // Extracts users attending a facebook event and exports them to a CSV file. 
 // Save the facebook event page to INPUT_PATH or replace source.html with the fb event page
 // results are saved to OUTPUT_PATH or "guest-list.csv"
 // and are in the format "User Id, name"
-function convertToCSV() {
+
+
+function convertToCSV(inputPath = "source.html") {
 	try {
-		const html = fs.readFileSync(INPUT_PATH).toString();
+		const html = fs.readFileSync(inputPath).toString();
 		const regex = /"event_connected_users_private.+]},"event_connected_emails_going"/;
 		const found = html.match(regex);
 		if (found.length != 1) {
@@ -39,13 +37,14 @@ function getUsers(data) {
 		return false;
 	}
 	_writeToFile(users);
+	console.log("Success");
 	return true;
 }
 
 // takes a JSON list of users and outputs their info into OUTPUT_PATh csv file
 function _writeToFile(users) {
 	const header = "User ID,Name\n";
-	let file = fs.createWriteStream(OUTPUT_PATH);
+	let file = fs.createWriteStream('guest-list.csv');
 	file.write(header);
 
 	users.forEach(function(user) {
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 #! /usr/bin/env node
 
-const guestList = require('./guest-list-to-csv.js')
+const yargs = require("yargs");
+const guestList = require('./guest-list-to-csv.js');
+
+const usage = "\nUsage: 'fbevents <optional source html file path>' to convert a guest list to .csv file";
+const options = yargs  
+      .usage(usage)                                                                                                   
+      .help(true)  
+      .argv;
 
 // RUN:
-guestList.convertToCSV();
+let sourcePath = "source.html";
+if (yargs.argv._[0]) sourcePath = yargs.argv._[0];
+guestList.convertToCSV(sourcePath);
